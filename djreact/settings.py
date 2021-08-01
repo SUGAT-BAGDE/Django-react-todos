a23e8897f1a2fdf699ec63e5bce04289e9c22089
Sugat Kishor Bagde <bagdesugat.2006@gmail.com>
2021-08-01 20:39:27 +0530

    initail

--- djreact/settings.py
+++ djreact/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,9 +24,9 @@ BASE_DIR = BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'django-insecure-h35yx@i*btrwg&!^y6#g1!uix(7kta(27yjzpqko=&ph_3##h+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','todolistsugat.herokuapp.com']
 
 
 # Application definition
@@ -45,7 +46,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djreact.urls'
@@ -147,10 +148,3 @@ CORS_ORIGIN_ALLOW_ALL=True
 CORS_ALLOW_HEADERS = [
     'fromreact',
 ]
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend/build/static'),
-)
-
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
