f7beec8b27882b50b33e9463344fd8d51affc5eb
SUGAT-BAGDE <79888221+SUGAT-BAGDE@users.noreply.github.com>
2021-08-02 13:32:54 +0530

    Update settings.py

--- djreact/settings.py
+++ djreact/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h35yx@i*btrwg&!^y6#g1!uix(7kta(27yjzpqko=&ph_3##h+'
+SECRET_KEY = '******************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
